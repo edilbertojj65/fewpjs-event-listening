6e402d363e99501442975fa68c984f283077767a
edilbertojj65 <edilbertojj65@gmail.com>
2021-04-06 18:04:59 -0400

    completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+ //   return "Clicked"
+ alert('I was clicked!');
+  }  );
+  
 }
+addingEventListener();
